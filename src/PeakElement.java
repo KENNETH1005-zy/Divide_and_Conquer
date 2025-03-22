3ef8936083c47dd37425491278b0b1253d372736
lizhengyuan <sli695439@gmail.com>
2025-03-22 19:37:30 -0400

    new

--- src/PeakElement.java
+++ src/PeakElement.java
@@ -1,23 +1,35 @@
-import java.util.*;
+import java.util.Arrays;
 
-
-class EuclideanAlgorithm {
-    public static int GCD(int a, int b) {
-        int result = 0;
-        while (a > 0 && b > 0) {
-            for (int i = 1; i <= a && i <= b; i++) {
-                if (a % i == 0 && b % i ==0) {
-                    result = i;
-                }
-            }
-            return result;
+class PeakElement {
+    public static int findPeakRec(int low, int high, int size, int[] array) {
+        int middle = low + (high - low) / 2;
+        if ((middle == size - 1 || array[middle + 1] <= array[middle]) &&
+                (middle == 0 || array[middle - 1] <= array[middle])) {
+            return middle;
         }
-        return -1;
+        if (array[middle-1] > array[middle] && middle >0) {
+            return findPeakRec(low, middle - 1, size, array);
+        }
+        else {
+            return findPeakRec(middle + 1, high, size, array);
+        }
     }
-    public static void main(String args[]) {
-        int a = 252;
-        int b = 105;
-        int number = GCD(a,b);
-        System.out.println(number);
+    public static int findPeak(int arr[]) {
+        int n = arr.length;
+        return findPeakRec(0, n-1, n, arr);
+    }
+    public static void main (String args[]) {
+        int[][] inputs = {{7, 11, 22, 13, 4, 0} , {13, 27, 54, 11, 99, 1} ,{0, 1, 2, 3, 4, 5} , {10, 9, 8, 7, 6}};
+        int peak = 0;
+        for (int i = 0; i < inputs.length; i++) {
+            peak = findPeak(inputs[i]);
+            if (i == 2)
+                System.out.println("Input Array: " + Arrays.toString(inputs[i]) + " ----> Peak = \"" + inputs[i][peak] + "\"\t\t (Ascending Order - Peak = Last Element)\n");
+            else if (i == 3)
+                System.out.println("Input Array: " + Arrays.toString(inputs[i]) + " ----> Peak = \"" + inputs[i][peak] + "\"\t\t(Descending Order - Peak = First Element)\n");
+            else
+                System.out.println("Input Array: " + Arrays.toString(inputs[i]) + " ----> Peak = \"" + inputs[i][peak] + "\"\n");
+
+        }
     }
 }
